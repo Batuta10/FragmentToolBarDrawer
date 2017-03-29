24e3613575c51fe11381e6fe784c1b14c230d2fe
Batuta10 <batuta2604@gmail.com>
2017-03-29 10:21:56 -0300

    Fragment Create

---
+++ app/src/main/java/br/com/fiap/exercicios/drawerandfragments/RM78792/GtoFragment.java
@@ -0,0 +1,22 @@
+package br.com.fiap.exercicios.drawerandfragments.RM78792;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class GtoFragment extends Fragment {
+
+    public GtoFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_gto, container, false);
+    }
+}
